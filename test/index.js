ea9a2b9b8885198cf0dbba93ecd9990eb5c89ed9
egg <egg@madsq.net>
2015-06-10 00:13:13 +0900

    fix not found error.

--- test/index.js
+++ test/index.js
@@ -8,11 +8,11 @@ ec2meta.load(function(err, categories) {
 });
 
 // // load meta data
-// ec2meta.load('instance-id', function(err, value) {
-//     console.log(value);
-// });
+ec2meta.load('instance-id', function(err, value) {
+    console.log(err, value);
+});
 
-// // load invalid meta data
-// ec2meta.load('instance-id-', function(err, value) {
-//     console.log(value);
-// });
+// load invalid meta data
+ec2meta.load('instance-id-', function(err, value) {
+    console.log(err, value);
+});
